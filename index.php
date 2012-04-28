8f35790b8ffa3f7c9fb0bda9393691af8007ec0a
Justin Hernandez <justin@transphorm.com>
2012-04-27 16:11:40 -1000

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+require('functions.php');
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  	<title>Bingo!</title>
+  	<link href="style.css" media="all" rel="stylesheet" type="text/css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+</head>
+<body>
+	
+	<div id="bingo">
+		<table>
+			
+		</table>
+	</div>
+
+</body>
+</html>
